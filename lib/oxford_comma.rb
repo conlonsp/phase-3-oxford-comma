f6242eb0fe6dc386de0f18d86d65a0da384066ef
samuelconlon <conlonsp@yahoo.com>
2022-12-07 14:47:31 -0500

    commit tree 0750ffa7d697bf93754ccb6099d562fbb0d3529f

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length <= 2
+    array.join(' and ')
+  else array.length > 2
+    last_e = array.pop
+    array << "and #{last_e}"
+    array.join(', ')
+  end
 end
